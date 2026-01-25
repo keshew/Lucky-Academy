cf01073de2899b698dd37bf04ec51d5ab956778d
keshew <artyom.korotckov2017@yandex.ru>
2026-01-25 15:32:34 +0500

    Initial Commit

---
+++ Mythic Path Collection Arcade/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mythic Path Collection Arcade
+//
+//  Created by Артём Коротков on 25.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mythic Path Collection Arcade/Mythic_Path_Collection_ArcadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mythic_Path_Collection_ArcadeApp.swift
+//  Mythic Path Collection Arcade
+//
+//  Created by Артём Коротков on 25.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Mythic_Path_Collection_ArcadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
